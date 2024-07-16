b308605341438a7abbd8fffe3bf71de6d1ed1cf8
dxvsh <vashishtdakksh@gmail.com>
2024-07-16 23:02:10 +0530

    Add support for updating and deleting notes

--- backend/app/services/notes_service.py
+++ backend/app/services/notes_service.py
@@ -13,3 +13,20 @@ class NoteService:
     def get_notes_by_student_id(student_id):
         return Note.query.filter_by(student_id=student_id).all()
     
+    @staticmethod
+    def get_note_by_id(note_id):
+        return Note.query.get(note_id)
+    
+    @staticmethod
+    def update_note(note_id, note_content):
+        note = Note.query.get(note_id)
+        note.note_content = note_content
+        db.session.commit()
+        return note
+    
+    @staticmethod
+    def delete_note(note_id):
+        note = Note.query.get(note_id)
+        db.session.delete(note)
+        db.session.commit()
+        return None # after deletion, there is no note to return
