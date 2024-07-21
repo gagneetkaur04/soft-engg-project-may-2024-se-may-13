869e136eb9180955499d602a81613efae9e3139f
Indrareddy5 <45683173+IndraReddy5@users.noreply.github.com>
2024-07-21 23:09:19 +0530

    created dummy components && initial css setup

--- frontend/padh.ai/src/router/index.js
+++ frontend/padh.ai/src/router/index.js
@@ -1,23 +1,101 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Course from '../views/Course.vue'
+import CourseHighlights from '../views/CourseHighlights.vue'
+import axios from 'axios'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/login',
+      name: 'base',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'login',
+      component: Login,
+      beforeEnter() {
+        if (localStorage.getItem('Auth-Token')) {
+          return '/dashboard'
+        }
+      }
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
+      beforeEnter() {
+        if (localStorage.getItem('Auth-Token')) {
+          return '/dashboard'
+        }
+      }
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      beforeEnter() {
+        if (!localStorage.getItem('Auth-Token')) {
+          return '/login'
+        }
+      } 
+    },
+    {
+      path: '/course/:courseId',
+      name: 'course',
+      component: Course,
+      props: ((route) => ({ contentId: route.params.contentId })),
+      beforeEnter() {
+        if (!localStorage.getItem('Auth-Token')) {
+          return '/login'
+        }
+      }
+    },
+    {
+      path: '/course/:courseId/highlights',
+      name: 'highlights',
+      component: CourseHighlights,
+      beforeEnter() {
+        if (!localStorage.getItem('Auth-Token')) {
+          return '/login'
+        } 
+      }
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      async beforeEnter() {
+        let request = {
+          url: __API_URL__ + 'logout',
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('Auth-Token')}`,
+            'content-type': 'application/json'
+          }
+        }
+        let response = await axios(request)
+        if (response.status === 200) {
+          localStorage.removeItem('Auth-Token')
+          return '/login'
+        }
+        else {
+          alert('Logout failed returning to dashboard')
+          return '/dashboard'
+        }
+      }
     }
   ]
 })
 
 export default router
+
+// login
+// signup
+// student dashboard
+// course homepage & content_id
+// course highlights
+// logout
---
+++ frontend/padh.ai/src/views/Course.vue
@@ -0,0 +1,7 @@
+<template>
+<h2>Course</h2>
+</template>
+<script>
+</script>
+<style>
+</style>
