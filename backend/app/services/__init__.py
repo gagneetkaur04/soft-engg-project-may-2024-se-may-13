d0f4a474114fc11226e8bfc3ffda5c5be40a5485
dxvsh <vashishtdakksh@gmail.com>
2024-07-24 17:57:51 +0530

    Implement logic for chat APIs
    Implemented logic for fetching, creating, continuing and deleting user conversations

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -2,3 +2,4 @@ from .user_service import UserService
 from .notes_service import NoteService
 from .course_service import CourseService
 from .assignment_service import AssignmentService
+from .chat_service import ChatService
