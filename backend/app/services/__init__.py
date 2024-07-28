125e536767ef42c469557e977dee677b710dd27b
dxvsh <vashishtdakksh@gmail.com>
2024-07-28 11:39:17 +0530

    Implement logic for handling programming assignments

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -3,3 +3,4 @@ from .notes_service import NoteService
 from .course_service import CourseService
 from .assignment_service import AssignmentService
 from .chat_service import ChatService
+from .prog_assignment_service import ProgrammingAssignmentService
