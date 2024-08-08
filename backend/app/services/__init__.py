f1077552b53a2d30d02c62a6832cb8b7a0c24fb3
dxvsh <vashishtdakksh@gmail.com>
2024-08-08 22:28:00 +0530

    Implement logic for generating lecture summaries

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -4,3 +4,4 @@ from .course_service import CourseService
 from .assignment_service import AssignmentService
 from .chat_service import ChatService
 from .prog_assignment_service import ProgrammingAssignmentService
+from .summary_service import SummaryService
