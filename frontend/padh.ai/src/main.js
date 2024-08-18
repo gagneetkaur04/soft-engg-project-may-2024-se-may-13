cf958c40dfb975bfef3d4f9760ebea32a3a26bf7
Indra Reddy <45683173+IndraReddy5@users.noreply.github.com>
2024-08-18 18:20:35 +0530

    Revert "Frontend Branch Merge (#22)"
    This reverts commit 1c9a25387f481138b698818835167f0e2106a70b.

--- frontend/padh.ai/src/main.js
+++ frontend/padh.ai/src/main.js
@@ -1,8 +1,5 @@
 import './assets/main.css'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
