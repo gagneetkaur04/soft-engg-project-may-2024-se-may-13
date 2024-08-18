1c9a25387f481138b698818835167f0e2106a70b
Indra Reddy <45683173+IndraReddy5@users.noreply.github.com>
2024-08-18 18:10:03 +0530

    Frontend Branch Merge (#22)
    * created dummy components && initial css setup
    
    * Vuelidate added
    
    * Login & Signup added
    
    * signup and login page modified // logout functionality added // top navbar is added on dashboard (#9)
    
    * Added dummy template design for chat window (#10)
    
    * signup and login page modified // logout functionality added // top navbar is added on dashboard
    
    * added course page, and chat window dummy template
    
    * code editor added and sideNavBar links for lectures are set (#11)
    
    * signup and login page modified // logout functionality added // top navbar is added on dashboard
    
    * added course page, and chat window dummy template
    
    * code editor added and sidenavbar links for lectures are set
    
    * added Dashboard vue
    
    * Frontend - modified CSS elements (#17)
    
    * added Dashboard vue
    
    * modified css elements - course, navbar, dashboard & login
    
    ---------
    
    Co-authored-by: Indra Reddy <45683173+IndraReddy5@users.noreply.github.com>
    
    * ACE Editor added (#18)
    
    * signup and login page modified // logout functionality added // top navbar is added on dashboard
    
    * added course page, and chat window dummy template
    
    * code editor added and sidenavbar links for lectures are set
    
    * assignment forms done
    
    * transcript.vue deleted
    
    * highlights finalized - menu item
    
    * added keypoints & notes menu (#20)
    
    * added keypoints & notes menu
    
    * added navbar toggle and modified login, signup & keypoint component
    
    ---------
    
    Co-authored-by: Shashwat Sunder <140502792+shazwat@users.noreply.github.com>

---
+++ frontend/padh.ai/src/components/NavBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <header class="navbar navbar-dark sticky-top topnav flex-md-nowrap p-0 shadow">
+    <router-link to="/dashboard" class="navbar-brand col-md-3 col-lg-2 me-0 px-4 fs-4 d-flex">
+      <img src="../assets/padhai_logo.png" alt="padhai" width="50" height="50">
+      <span style="align-self: flex-end;">PADH.AI</span>
+    </router-link>
+    <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse"
+      data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation" v-if="showToggle">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-nav topnav_user">
+      <div class="nav-item text-nowrap">
+        <div class="dropdown px-3 float-end">
+          <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+            id="dropdownUser" data-bs-toggle="dropdown" aria-expanded="false">
+            <strong>{{ email }}</strong>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser"
+            data-popper-placement="bottom-end"
+            style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(15px, 34px, 0px);">
+            <router-link to="/logout" class="dropdown-item">logout</router-link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: 'NavBar',
+  data: function () {
+    return {
+      email: localStorage.getItem("email"),
+      Auth_Token: localStorage.getItem("Auth-Token"),
+    }
+  },
+  props: {
+    showToggle: {
+      type: Boolean,
+      default: true,
+    }
+
+  },
+}
+</script>
+<style>
+.topnav {
+  background-color: var(--dark-green);
+}
+
+.navbar {
+  z-index: 25;
+}
+
+.navbar-brand {
+  padding-top: .75rem;
+  padding-bottom: .75rem;
+  font-family: 'Roboto', sans-serif;
+}
+
+.navbar-brand a:hover {
+  text-decoration: none;
+  background-color: none;
+}
+
+.navbar .navbar-toggler {
+  top: .25rem;
+  right: 1rem;
+}
+
+@media (max-width:768px) {
+  .topnav_user {
+    width: 100% !important
+  }
+}
+</style>
---
+++ frontend/padh.ai/src/views/Signup.vue
@@ -0,0 +1,254 @@
+<template>
+  <div class="box">
+    <div class="board">
+      <div class="container board_body row m-auto">
+        <div class="col-md-6 col-sm-12">
+          <div class="brand_space">
+            <div class="logo">
+              <img src="@/assets/padhai.png" alt="logo">
+            </div>
+            <div class="brand_text">
+              <div>
+                <h2>Create Account</h2>
+                <span>to access padhai</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-sm-12">
+          <form method="POST" @submit.prevent="handleFormSubmit" class="float-right">
+            <div class="form-floating mb-3">
+              <input type="text" :class="{ 'form-control': true, 'is-invalid': v$.first_name.$error }"
+                v-model="first_name" name="first_name" id="floatingInput1" placeholder="first_name"
+                autocomplete="off" />
+              <label for="floatingInput1">first_name</label>
+              <div class="invalid-feedback" style="color: #dc3545 !important" v-if="v$.first_name.$error">
+                <span>{{ v$.first_name.$errors[0].$message }}</span>
+              </div>
+            </div>
+            <div class="form-floating mb-3">
+              <input type="text" :class="{ 'form-control': true, 'is-invalid': v$.last_name.$error }"
+                v-model="last_name" name="last_name" id="floatingInput2" placeholder="last_name" autocomplete="off" />
+              <label for="floatingInput2">last_name</label>
+              <div class="invalid-feedback" style="color: #dc3545 !important" v-if="v$.last_name.$error">
+                <span>{{ v$.last_name.$errors[0].$message }}</span>
+              </div>
+            </div>
+            <div class="form-floating mb-3">
+              <input type="email" :class="{ 'form-control': true, 'is-invalid': v$.email.$error }" v-model="email"
+                name="email" id="floatingInput3" placeholder="email" autocomplete="off" />
+              <label for="floatingInput3">email</label>
+              <div class="invalid-feedback" style="color: #dc3545 !important" v-if="v$.email.$error">
+                <span>{{ v$.email.$errors[0].$message }}</span>
+              </div>
+            </div>
+            <div class="form-floating mb-3">
+              <input type="password" :class="{ 'form-control': true, 'is-invalid': v$.password.$error }"
+                v-model="password" name="password" id="floatingPassword1" placeholder="Password" autocomplete="off" />
+              <label for="floatingPassword1">Password</label>
+              <div class="invalid-feedback" style="color: #dc3545 !important" v-if="v$.password.$error">
+                <span>{{ v$.password.$errors[0].$message }}</span>
+              </div>
+            </div>
+            <div class="form-floating mb-3">
+              <input type="password" :class="{
+                'form-control': true,
+                'is-invalid': v$.repeatPassword.$error,
+              }" v-model="repeatPassword" name="repeat_password" id="floatingPassword2"
+                placeholder="repeat the same password as above" autocomplete="off" />
+              <label for="floatingPassword2">Repeat Password</label>
+              <div class="invalid-feedback" style="color: #dc3545 !important" v-if="v$.repeatPassword.$error">
+                <span>{{ v$.repeatPassword.$errors[0].$message }}</span>
+              </div>
+            </div>
+            <button class="w-100 btn btn-lg btn-submit" type="submit">
+              Sign Up
+            </button>
+            <hr class="my-4" />
+            <div class="alert alert-danger alert-dismissible fade show" role="alert" v-if="errStatus">
+              <strong>{{ errormsg }}</strong>.
+              <button type="button" class="btn-close" aria-label="Close" @click="errStatus = false"></button>
+            </div>
+            <small>Have an account? <router-link to="/login">Login now</router-link>
+            </small>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useVuelidate } from "@vuelidate/core";
+import {
+  required,
+  alphaNum,
+  email,
+  sameAs,
+  helpers,
+} from "@vuelidate/validators";
+import axios from "axios";
+export default {
+  setup() {
+    return {
+      v$: useVuelidate(),
+    };
+  },
+  name: "SignUp",
+  data: function () {
+    return {
+      first_name: "",
+      last_name: "",
+      email: " ",
+      password: "",
+      repeatPassword: "",
+      errormsg: "",
+      errStatus: false,
+    };
+  },
+  validations() {
+    return {
+      first_name: {
+        required: helpers.withMessage(
+          "The first_name field is required",
+          required
+        ),
+        alphaNum: helpers.withMessage(
+          "The first_name must contain only letters and numbers",
+          alphaNum
+        ),
+      },
+      last_name: {
+        required: helpers.withMessage(
+          "The last_name field is required",
+          required
+        ),
+        alphaNum: helpers.withMessage(
+          "The last_name must contain only letters and numbers",
+          alphaNum
+        ),
+      },
+      email: {
+        required: helpers.withMessage("The email field is required", required),
+        email: helpers.withMessage("The email must be valid", email),
+      },
+      password: {
+        required: helpers.withMessage(
+          "The password field is required",
+          required
+        ),
+      },
+      repeatPassword: {
+        required: helpers.withMessage(
+          "The confirm password field is required",
+          required
+        ),
+        sameAsPassword: helpers.withMessage(
+          "The passwords should match",
+          sameAs(this.password)
+        ),
+      },
+    };
+  },
+  methods: {
+
+    async handleFormSubmit() {
+      this.v$.$touch();
+      if (this.first_name) {
+        let payload = {
+          first_name: this.first_name,
+          last_name: this.last_name,
+          email: this.email,
+          password: this.password,
+        }
+        let request = {
+          url: __API_URL__ + "auth/register",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          data: JSON.stringify(payload),
+        };
+        // let response = await axios(request)
+        // console.log(response)
+        // alert(response.data)
+        await axios(request).then((response) => {
+          console.log(response);
+          alert("User registered successfully, redirecting to login page");
+          this.$router.push("/login");
+        })
+          .catch((error) => {
+            console.log(error.response.data.errStatus);
+            this.errormsg = error.response.data.message;
+            this.errStatus = true;
+          });
+      }
+    }
+  }
+};
+</script>
+<style>
+body {
+  display: grid;
+  place-items: center;
+  background-color: var(--light-green);
+  height: 100%;
+  margin: 0;
+}
+
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0 5em 0 5em;
+}
+
+.board {
+  background-color: var(--ivory);
+  border-radius: 2em;
+  box-shadow: 5px 5px 15px 2px var(--dark-green);
+  width: 100%;
+  padding: 1em;
+  text-align: center;
+}
+
+.board_body {
+  display: flex !important;
+  justify-content: space-between !important;
+  align-items: center;
+  padding: 1em;
+}
+
+.brand_space {
+  flex: 1;
+  padding-right: 2em;
+  /* Takes up equal space */
+}
+
+.brand_text {
+  display: flex;
+  padding-top: 2em;
+  justify-content: center;
+}
+
+form {
+  flex: 2;
+  /* Takes up twice the space of brand_space */
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  max-width: 300px;
+}
+
+h2 {
+  margin: 0;
+  font-size: 1.5em;
+}
+</style>
