abfc3ce1c42d6ceab38b59f1ef8e550f810656f2
Indra Reddy <45683173+IndraReddy5@users.noreply.github.com>
2024-08-18 18:20:20 +0530

    Revert "Backend Final Merge (#23)" (#24)
    This reverts commit c8e640bb9892910b4e5f73ca25d95495537ffefe.

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .student_models import Student
-from .course_models import Course, CourseContent, Enrollment, Instructor
-from .assignment_models import Assignment, AssignmentQuestion, SubmissionAnswer, Grade
-from .misc_models import Note, Conversation, ChatMessage
-from .prog_assignment_models import ProgrammingAssignment, ProgrammingSubmission, TestCase
